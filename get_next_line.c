4a91e6258b9d453fcc18ffe74ed3f3a7cf6960b6
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-07-01 16:32:59 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,45 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouvier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/01 14:33:59 by ebouvier          #+#    #+#             */
+/*   Updated: 2018/07/01 14:55:43 by ebouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-
-
-char	*ft_strnew(size_t size)
-{
-	char *s;
-
-	s = (char*)malloc(sizeof(*s) * (size + 1));
-	if (!s)
-		return (NULL);
-	memset(s, 0, size + 1);
-	return (s);
-}
-
-char	*ft_strjfree(char *s1, const char *s2)
-{
-	size_t	count;
-	size_t	len[2];
-	char	*str;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len[0] = strlen(s1);
-	len[1] = strlen(s2);
-	str = ft_strnew(len[0] + len[1]);
-	if (str == NULL)
-		return (NULL);
-	count = 0;
-	while (count < len[0])
-	{
-		str[count] = s1[count];
-		count++;
-	}
-	while (count < (len[0] + len[1]))
-	{
-		str[count] = s2[count - len[0]];
-		count++;
-	}
-	str[count] = '\0';
-	free(s1);
-	return (str);
-}
 
 static char	*tokenstr(char *str)
 {
@@ -52,7 +23,7 @@ static char	*tokenstr(char *str)
 		i++;
 	s = &str[i + 1];
 	l = str;
-	str = strdup(s);
+	str = ft_strdup(s);
 	free(l);
 	return (str);
 }
@@ -64,44 +35,44 @@ static char	**tokenline(char **line, char *str)
 	i = 0;
 	while (str[i] != '\n')
 		i++;
-	*line = strndup(str, i);
+	*line = ft_strndup(str, i);
 	return (line);
 }
 
 static char	*buffertostr(char *str, char *buff, ssize_t bytes)
 {
 	if (!str)
-		str = strdup(buff);
-	else if (bytes != 0 && str && strchr(str, '\n') == 0)
+		str = ft_strdup(buff);
+	else if (bytes != 0 && str && ft_strchr(str, '\n') == 0)
 		str = ft_strjfree(str, buff);
 	return (str);
 }
 
-static int readline(char **stc, char **line, int fd, ssize_t bytes)
+static int	readline(char **stc, char **line, int fd, ssize_t bytes)
 {
 	if (bytes == 0 && *stc[fd] == '\0')
 		return (0);
-	else if (bytes == 0 && strchr(stc[fd], '\n') == 0)
+	else if (bytes == 0 && ft_strchr(stc[fd], '\n') == 0)
 	{
-		*line = strdup(stc[fd]);
-		bzero(stc[fd], strlen(stc[fd]));
+		*line = ft_strdup(stc[fd]);
+		ft_bzero(stc[fd], ft_strlen(stc[fd]));
 		return (1);
 	}
 	return (0);
 }
 
-int	get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	char		buff[BUFF_SIZE + 1];
 	ssize_t		bytes;
-	static char	*stc[FOPEN_MAX];
+	static char	*stc[OPEN_MAX];
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || read(fd, buff, 0) < 0)
 		return (-1);
 	bytes = 1;
-	while (bytes != 0)
+	while (1)
 	{
-		if (!stc[fd] || strchr(stc[fd], '\n') == 0)
+		if (!stc[fd] || ft_strchr(stc[fd], '\n') == 0)
 		{
 			bytes = read(fd, buff, BUFF_SIZE);
 			buff[bytes] = '\0';
@@ -109,11 +80,12 @@ int	get_next_line(const int fd, char **line)
 		if (bytes == 0)
 			return (readline(stc, line, fd, bytes));
 		stc[fd] = buffertostr(stc[fd], buff, bytes);
-		if ((strchr(stc[fd], '\n')) != 0)
+		if ((ft_strchr(stc[fd], '\n')) != 0)
 		{
 			line = tokenline(line, stc[fd]);
 			stc[fd] = tokenstr(stc[fd]);
 			return (1);
 		}
 	}
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouvier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/01 14:35:38 by ebouvier          #+#    #+#             */
+/*   Updated: 2018/07/01 14:54:14 by ebouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 4096
 
-
-#define BUFF_SIZE 4096
-
-int	get_next_line(const int fd, char **line);
-
+int	get_next_line(int fd, char **line);
 #endif
